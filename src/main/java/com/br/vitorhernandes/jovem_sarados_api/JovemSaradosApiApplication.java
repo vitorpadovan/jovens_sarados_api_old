0a1bab9fec925ca001ef54cff1c0a0e24573d5ac
Vitor Hugo Padovan Hernandes <vitor.padovan89@gmail.com>
2021-09-07 16:26:49 -0300

    Commit projeto inicial

---
+++ src/main/java/com/br/vitorhernandes/jovem_sarados_api/JovemSaradosApiApplication.java
@@ -0,0 +1,13 @@
+package com.br.vitorhernandes.jovem_sarados_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JovemSaradosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JovemSaradosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/vitorhernandes/jovem_sarados_api/JovemSaradosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.vitorhernandes.jovem_sarados_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JovemSaradosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

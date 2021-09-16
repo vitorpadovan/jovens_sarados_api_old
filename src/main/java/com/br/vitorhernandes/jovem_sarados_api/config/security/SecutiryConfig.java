6096f00c6f4fb1a160c73b4bfe832a1b020a7761
Vitor Hugo Padovan Hernandes <vitor.padovan89@gmail.com>
2021-09-16 19:55:37 -0300

    #3 - Implementar configurações JWT 1. implementado WebSecurity e remoção do CORS

---
+++ src/main/java/com/br/vitorhernandes/jovem_sarados_api/config/security/SecutiryConfig.java
@@ -0,0 +1,48 @@
+package com.br.vitorhernandes.jovem_sarados_api.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecutiryConfig extends WebSecurityConfigurerAdapter {
+
+	private static final String[] PUBLIC_GET = { "/api/evento" };
+
+	private static final String[] PUBLIC_POST = {};
+
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors().and().csrf().disable();
+		http.authorizeRequests().antMatchers(PUBLIC_GET).permitAll().antMatchers(PUBLIC_POST).permitAll().anyRequest()
+				.authenticated();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**");
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return source;
+	}
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
